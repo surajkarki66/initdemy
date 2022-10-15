ae6258c1d694c81236a0ef7d093477b25421fc3b
surajkarki66 <suraj.karki500@protonmail.com>
2022-10-15 22:10:55 +0545

    Modified account-settings

--- client/pages/user/account-settings.tsx
+++ client/pages/user/account-settings.tsx
@@ -207,7 +207,7 @@ const AccountSettings: NextPage = () => {
       <UserRoute>
         <div className="jumbotron text-center bg-primary square">
           <h1 style={{ color: "white" }}>Account Settings</h1>
-          <p className="lead">This is your account's different settings</p>
+          <p className="lead">This is your account&apos;s different settings</p>
         </div>
         <div>
           <Collapse
